3445240e614ab541d4c0310ba6cb32ac44c8df33
ngopy <muhalibakhtiar@gmail.com>
2022-07-13 21:44:15 +0700

    update infra api

--- app/app/model/product_model.py
+++ app/app/model/product_model.py
@@ -5,7 +5,7 @@ from sqlalchemy import (
     ForeignKey, Date, 
     DateTime, Numeric, 
     Boolean, Float,
-    TIMESTAMP
+    TIMESTAMP, Text,
     )
 from sqlalchemy.sql import func
 
@@ -13,10 +13,29 @@ class Products(Base):
 
     __tablename__ = 'products'
 
-    id: Column(String(36), primary_key = True)
-    name: Column(String(200))
-    description: Column(String(200))
-    images: Column(String(200))
-    logo_id: Column(Integer)
+    id = Column(String(36), primary_key = True)
+    name = Column(String(200))
+    description = Column(String(200))
+    images = Column(String(200))
+    logo_id = Column(Integer)
     create_at = Column(TIMESTAMP, server_default=func.now())    
-    update_at: Column(TIMESTAMP) 
+    update_at = Column(TIMESTAMP) 
+
+class Variant(Products):
+    __tablename__ = 'variants'
+
+    id = Column(String(36), primary_key = True)
+    product_id = Column(BigInteger, ForeignKey("products.id"), autoincrement=True)
+    name = Column(String(100))
+    size = Column(Integer)
+    color = Column(String(100))
+    images = Column(String(100))
+    create_at = Column(TIMESTAMP, server_default=func.now())    
+    update_at = Column(TIMESTAMP) 
+
+class Image(Base):
+    __tablename__ = 'variants'
+
+    id = Column(String(36), primary_key = True)
+    url = Column(Text)
+
